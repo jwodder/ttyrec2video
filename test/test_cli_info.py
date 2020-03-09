ae7a2391a5ff9e8b9cc0bbe54c3927627eba722d
John T. Wodder II <git@varonathe.org>
2020-03-09 19:39:25 +0000

    Handle test breakage from new Click version

--- test/test_cli_info.py
+++ test/test_cli_info.py
@@ -1,6 +1,7 @@
-from pathlib               import Path
-from click.testing         import CliRunner
-from ttyrec2video.__main__ import main
+from   pathlib               import Path
+import re
+from   click.testing         import CliRunner
+from   ttyrec2video.__main__ import main
 
 DATA_DIR = Path(__file__).with_name('data')
 
@@ -34,10 +35,15 @@ def test_cli_info_short():
     pth = str(DATA_DIR / 'short.ttyrec')
     r = CliRunner().invoke(main, ['--info', pth])
     assert r.exit_code != 0
-    assert r.output == '''\
-Reading {} ...
-Usage: ttyrec2video [OPTIONS] TTYREC [OUTFILE]
-Try "ttyrec2video -h" for help.
-
-Error: ttyrec update at offset 18 ended prematurely; expected 14 bytes, got 6
-'''.format(pth)
+    stdout_lines = r.output.splitlines()
+    assert len(stdout_lines) == 5, r.output
+    assert stdout_lines[0] == 'Reading {} ...'.format(pth)
+    assert stdout_lines[1] == 'Usage: ttyrec2video [OPTIONS] TTYREC [OUTFILE]'
+    assert re.match(
+        r'^Try [\'"]ttyrec2video -h[\'"] for help\.$',
+        stdout_lines[2],
+    )
+    assert stdout_lines[3] == ''
+    assert stdout_lines[4] \
+        == 'Error: ttyrec update at offset 18 ended prematurely;'\
+           ' expected 14 bytes, got 6'
